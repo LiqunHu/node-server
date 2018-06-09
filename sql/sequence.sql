4e886c4061cff22034819b2f334d172dea68c189
LiqunHu <huliquns@126.com>
2018-06-09 13:33:12 +0800

    fix

--- sql/sequence.sql
+++ sql/sequence.sql
@@ -1,13 +1,21 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50722
+ Source Host           : localhost
+ Source Database       : mvnndata
+
+ Target Server Type    : MySQL
+ Target Server Version : 50722
+ File Encoding         : utf-8
+
+ Date: 06/09/2018 13:13:17 PM
+*/
+
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
-
-CREATE DATABASE IF NOT EXISTS mvnndata DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-
-use mvnndata;
-
--- ----------------------------
---  Table structure for `seqmysql`
--- ----------------------------
 DROP TABLE IF EXISTS `seqmysql`;
 CREATE TABLE `seqmysql` (
   `seqname` varchar(50) NOT NULL,
@@ -15,20 +23,44 @@ CREATE TABLE `seqmysql` (
   `increment` int(11) NOT NULL DEFAULT '1',
   `max` bigint(20) NOT NULL DEFAULT '99999999',
   PRIMARY KEY (`seqname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
+
+INSERT INTO `seqmysql` VALUES ('userIDSeq', '0', '1', '99999999');
 
 -- ----------------------------
---  Records of `seqmysql`
+--  Procedure structure for `Pro_AddMenu`
 -- ----------------------------
-BEGIN;
-INSERT INTO `seqmysql` VALUES ('userIDSeq', '0', '1', '99999999');
-COMMIT;
+DROP PROCEDURE IF EXISTS `Pro_AddMenu`;
+delimiter ;;
+CREATE PROCEDURE `Pro_AddMenu`(IN pmenuName VARCHAR(100), IN fmenuName VARCHAR(100), IN funcPath VARCHAR(100), IN funcName VARCHAR(100))
+BEGIN
+	DECLARE v_fmenu_id BIGINT;
+
+	DECLARE v_api_id BIGINT;
+
+	DECLARE v_api_name VARCHAR(300);
+
+	DECLARE v_api_function VARCHAR(100);
+
+	SELECT systemmenu_id INTO v_fmenu_id FROM tbl_common_systemmenu WHERE systemmenu_name = pmenuName;
+
+	INSERT INTO tbl_common_api(api_name , api_kind , api_path, api_function, auth_flag, show_flag, created_at, updated_at)
+	VALUES(fmenuName , '1' , funcPath, funcName, '1', '1', now(), now());
+
+	SELECT api_id , api_name , api_function INTO v_api_id , v_api_name , v_api_function FROM tbl_common_api WHERE api_function = funcName;
+
+	INSERT INTO tbl_common_systemmenu( systemmenu_name , api_id , api_function , node_type , parent_id , created_at , updated_at) VALUES( v_api_name , v_api_id , v_api_function , '01' , v_fmenu_id , now() , now());
+
+END
+ ;;
+delimiter ;
 
 -- ----------------------------
 --  Function structure for `currval`
 -- ----------------------------
 DROP FUNCTION IF EXISTS `currval`;
 delimiter ;;
+CREATE FUNCTION `currval`(seq_name VARCHAR(50)) RETURNS int(11)
 BEGIN
   DECLARE current INTEGER;
   SET current = 0;
@@ -72,3 +104,5 @@ BEGIN
 END
  ;;
 delimiter ;
+
+SET FOREIGN_KEY_CHECKS = 1;
