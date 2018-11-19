7ba62d685a2b60b64954c30923302f5b9b9f1258
huliqun <huliquns@126.com>
2018-11-19 18:22:47 +0800

    增加默认超时处理

--- util/RPCServerClient.js
+++ util/RPCServerClient.js
@@ -40,19 +40,29 @@ exports.ServerRequest = (server, url, message) => {
     RPCPools[server].pool
       .acquire()
       .then(function(ws) {
+        const timeoutHandle = setTimeout(function() {
+          RPCPools[server].pool.destroy(ws)
+          reject({
+            errno: -2,
+            msg: 'time out',
+            info: {}
+          })
+        }, 5000) // 默认超时时间5s
+
         function incomingHandler(msg) {
           // logger.info(RPCPools[server].pool.available)
           // logger.info(RPCPools[server].pool.size)
           // logger.info(RPCPools[server].pool.borrowed)
           RPCPools[server].pool.release(ws)
           ws.removeListener('message', incomingHandler)
+          clearTimeout(timeoutHandle)
           resolve(JSON.parse(msg))
         }
         ws.on('message', incomingHandler)
         ws.on('error', function error(error) {
           logger.info('%s connected: %s', s, error)
           RPCPools[server].pool.destroy(ws)
-          reject(message)
+          reject(error)
         })
         ws.send(JSON.stringify({ url: url, message: message }))
       })
