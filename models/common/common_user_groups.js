f8ae884634f51c57990d5619d432856dea8a7a33
huliqun <huliquns@126.com>
2018-11-21 11:01:11 +0800

    f

---
+++ models/common/common_user_groups.js
@@ -0,0 +1,20 @@
+/* 用户所属组 */
+const db = require('../../util/db')
+const GLBConfig = require('../../util/GLBConfig')
+
+module.exports = db.defineModel('tbl_common_user_groups', {
+  user_groups_id: {
+    type: db.IDNO,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  user_id: {
+    type: db.IDNO,
+    allowNull: false
+  },
+  usergroup_id: {
+    // 用户组
+    type: db.IDNO,
+    allowNull: false
+  }
+})
